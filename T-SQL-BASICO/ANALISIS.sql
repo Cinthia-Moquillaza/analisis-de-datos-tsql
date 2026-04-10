8c82da93c27244d5a7974b4e39e4c56df043135e
Cinthia-Moquillaza <cmoquillaza@outlook.com>
2026-04-10 10:34:19 -0500

    Se agregan comentarios

--- T-SQL-BASICO/ANALISIS.sql
+++ T-SQL-BASICO/ANALISIS.sql
@@ -6,18 +6,21 @@ Descripción: Consulta a la tabla factura para obtener la sumatoria por estado
 **/
 
 
+--consulta total a la tabla factura y estado retirada
+
 SELECT 
 TOP 10 
 SUM (MontoTotal)
 
-
 FROM FACTURAS
 where estado = 'Retirada'
+go
+
+--consulta a la tabla factura para obtener los 10 primeros registros
 
 SELECT 
 
-
 TOP 10 *
 
-
 FROM FACTURAS
+go
